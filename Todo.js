df60626032030b280f22967f3f1d8c68f946722c
gab.abasolo <31750757+gabrielleabasolo@users.noreply.github.com>
2022-03-17 22:56:29 +0800

    Changed the whole project interface
    Completely changed the whole project

--- Todo.js
+++ Todo.js
@@ -1,30 +1,31 @@
-function ToDoMain(){
-    //get document data
-    var tbl = document.getElementById("myTable");
-    var row = tbl.insertRow(-1);
+function add(){
+    var task = document.getElementById("task").value;
+    var desc = document.getElementById("desc").value;
+    var start = document.getElementById("start").value;
+    var end = document.getElementById("end").value;
 
-    //set which table rows to insert
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
+    var table = document.getElementById("myTable");
 
-    //insert html
-    for(i = 0; i < tbl.rows.length; i++){
-        cell1.innerHTML = i;
-    }
-    cell2.innerHTML = document.innerHTML.createElement("INPUT");
-    cell3.innerHTML = document.innerHTML.createElement("INPUT");
-    cell4.innerHTML = document.innerHTML.createElement("INPUT");
-
-    cell2.setAttribute("type", "text");
-    cell3.setAttribute("type", "text");
-    cell4.setAttribute("type", "text");
+    var row = table.insertRow(0);
     
+    var myTask = row.insertCell(0);
+    var myDesc = row.insertCell(1);
+    var myStart = row.insertCell(2);
+    var myEnd = row.insertCell(3)
 
 
+    myTask.innerHTML = task;
+    myDesc.innerHTML = desc;
+    myStart.innerHTML = start;
+    myEnd.innerHTML = end;
+
+
+    document.getElementById("task").value = "";
+    document.getElementById("desc").value = "";
+    document.getElementById("start").value = "";
+    document.getElementById("end").value = "";
 }
 
-function AddElement(){
+function Delete(){
 
 }
